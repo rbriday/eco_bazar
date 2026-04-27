edaf3987f7fb8dc216719eff5817820af3b1496e
Riday Rajbongshi <ridaya100@gmail.com>
2026-04-27 12:20:57 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+
+app.get('/', (req, res)=>{
+    res.send("hello")
+});
+
+app.listen(9000, ()=>{
+    console.log("server Is Running")
+});
